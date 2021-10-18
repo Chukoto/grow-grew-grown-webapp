3e6aea04c572dba0c2b1349a6b73ef00724245e2
Haruki Hirai <iamprivateengineer@gmail.com>
2021-10-18 18:50:32 +0900

    [add] created GenreSwitcher

---
+++ src/components/GenreSwitcher.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-row>
+      <v-col class="d-flex" cols="6">
+        <v-select
+          :items="genresFromParent"
+          label="表示するジャンルを選択"
+          dense
+          outlined
+          absolute
+          v-model="selected"
+          @change="sendSelectedVal"
+        ></v-select>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GenreSwitcher',
+  props: {
+    genresFromParent: {
+      genres: Object,
+    },
+  },
+  data() {
+    return {
+      selected: '',
+    };
+  },
+  methods: {
+    sendSelectedVal() {
+      this.$emit('setSelectedVal', this.selected);
+    },
+  },
+};
+</script>
+
+<style></style>
