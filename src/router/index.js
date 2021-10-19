5800d13e08dff8f91aca34e5bbcec8c952bdde61
Haruki Hirai <iamprivateengineer@gmail.com>
2021-10-20 00:26:44 +0900

    [mod] imported progress to router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Target from '@/pages/Target';
+import Progress from '@/pages/Progress';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/target',
     name: 'Target',
     component: Target,
+  },
+  {
+    path: '/progress',
+    name: 'Progress',
+    component: Progress,
   },
 ];
 
